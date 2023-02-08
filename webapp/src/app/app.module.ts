43d5e479a673611679bb4309230989f54f12d65f
leonleonardo <leonleonardo@mailbox.org>
2023-02-08 10:37:05 -0500

    general updating filepaths

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -5,22 +5,37 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule} from './material/material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+
 import { NavbarComponent } from './navbar/navbar.component';
 
+import { HomeComponent } from './home/home.component';
+import { DateRangePickerComponent } from './components/date-range-picker';
+
+import { LoginComponent } from './login/login.component';
+
+import { TripsComponent } from './trips/trips.component';
+import { TripsCardComponent } from './trips/card.component';
+import { TripsCardDeckComponent } from './trips/deck.component';
+
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     HomeComponent,
+    DateRangePickerComponent,
     LoginComponent,
-    NavbarComponent 
+    TripsComponent,
+    TripsCardComponent,
+    TripsCardDeckComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +43,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HttpClientModule,
     AppRoutingModule,
     MaterialModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
