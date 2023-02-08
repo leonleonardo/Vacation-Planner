106ff0f0376020b46da865ae0cded1b0ce6222f8
benmendoza3 <b.mendoza@ufl.edu>
2023-02-08 02:25:52 -0500

    added username validation!

--- server/routes/CreateUser.go
+++ server/routes/CreateUser.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"vacation-planner/models"
-
 	"github.com/gorilla/mux"
 )
 
@@ -13,18 +12,30 @@ import (
 func (h DBRouter) CreateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	username := params["username"]
+	// Changed this to not create a mock user until we have checked
+	// the DB for any users with the given username
 	var user models.User
 
-	user.Firstname = "Kendy"
-	user.Lastname = "Phillips"
-	user.Username = username
-	user.Password = "1234"
+	// Added a line to check the database for any users with the same username
+	// as the new account
+	result := h.DB.First(&models.User{}, "Username = ?", params["username"])
 
-	// Creating new user and checking for error
-	if result := h.DB.Create(&user); result.Error != nil {
-		fmt.Println(result.Error)
+	// Checking if the rows that have the username is 0 therefore nobody has the username
+	// still using mock information until figuring out how to work with http request BODY(?)
+	if result.RowsAffected == 0 {
+		user.Firstname = "Top"
+		user.Lastname = "G"
+		user.Username = params["username"]
+		user.Password = "blackPowerRanger"
+	
+		// Creating new user and checking for error
+		if newUser := h.DB.Create(&user); newUser.Error != nil {
+			fmt.Println(newUser.Error)
+		}
+
+		json.NewEncoder(w).Encode(user)
+
+	} else {
+		w.Write([]byte("Username taken!"))
 	}
-
-	json.NewEncoder(w).Encode(user)
 }
