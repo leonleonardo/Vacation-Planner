1a51159571a5cf756f747ba41879f4a465c71060
leonleonardo <leonleonardo@mailbox.org>
2023-02-08 10:34:40 -0500

    Added trips page

---
+++ webapp/src/app/trips/card.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'trips-card-component',
+  templateUrl: 'card.component.html',
+  styleUrls: ['card.component.scss'],
+})
+export class TripsCardComponent {}
