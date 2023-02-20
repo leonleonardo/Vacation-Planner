b5f27ce145f895cb5e4a5b172792c31f9fc5678e
benmendoza3 <b.mendoza@ufl.edu>
2023-02-19 22:40:41 -0500

    finished merge changes

--- server/models/models.go
+++ server/models/models.go
@@ -4,6 +4,22 @@ import (
 	"gorm.io/gorm"
 )
 
+type Business struct {
+    Name        string  `json: "name"`
+    Price       string  `json: "price"`
+    Rating      float64 `json: "rating"`
+    Location    string  `json: "location"`
+    Type        string  `json: "type"`
+}
+
+type Destination struct {
+    gorm.Model
+    Location        [3]string        `json: "location"`
+    Restaurants     [10]Business    `json: "restaurants"`
+    Entertainment   [10]Business    `json: "entertainment"`
+    Shopping        [10]Business    `json: "shopping"`
+}
+
 // introducing user type for database
 type User struct {
 	gorm.Model
