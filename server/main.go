d3a60b55c7b96baad782af326c1e330c40333cae
benmendoza3 <b.mendoza@ufl.edu>
2023-02-19 22:37:01 -0500

    merged so far changes for back-end into REST API branch

--- server/main.go
+++ server/main.go
@@ -1,30 +1,35 @@
 package main
 
 import (
-    "net/http"
-    "log"
-    "github.com/gorilla/mux"
-    //"encoding/json"
-    //"github.com/jmatth11/yfusion"
-    //"fmt"
-    //"os"
-    //"github.com/joho/godotenv"
-    //"gorm.io/gorm"
+	"log"
+	"net/http"
+	"vacation-planner/database"
+	"vacation-planner/routes"
+
+	"github.com/gorilla/mux"
 )
 
 func YourHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Gorilla!\n"))
+	//test database connection for default route
+	//prints "error" on error, prints "successful connection" on success
+	database.Connect()
+	w.Write([]byte("Gorilla!\n"))
 }
 
 func main() {
+	// Establishing database connection
+	db := database.Connect()
 
-    r := mux.NewRouter()
-    // Routes consist of a path and a handler function.
-    r.HandleFunc("/", YourHandler)
+	// Packaging database connection to be passed to handlers
+	h := routes.NewConnection(db)
 
-    // New function handler for "GET" requests
-    r.HandleFunc("/newDestination/{location}", GetDestInfo).Methods("GET")
+	r := mux.NewRouter()
 
-    // Bind to a port and pass our router in
-    log.Fatal(http.ListenAndServe(":8080", r))
+	// Created test POST request for database
+	r.HandleFunc("/CreateUser/{username}", h.CreateUser).Methods("POST")
+
+	r.HandleFunc("/newDestination/{location}", h.GetDestInfo).Methods("GET")
+
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
---
+++ server/routes/CreateUser.go
@@ -0,0 +1,41 @@
+package routes
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"vacation-planner/models"
+	"github.com/gorilla/mux"
+)
+
+// Create user POST using mock data & parameter from the request
+func (h DBRouter) CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	// Changed this to not create a mock user until we have checked
+	// the DB for any users with the given username
+	var user models.User
+
+	// Added a line to check the database for any users with the same username
+	// as the new account
+	result := h.DB.First(&models.User{}, "Username = ?", params["username"])
+
+	// Checking if the rows that have the username is 0 therefore nobody has the username
+	// still using mock information until figuring out how to work with http request BODY(?)
+	if result.RowsAffected == 0 {
+		user.Firstname = "Top"
+		user.Lastname = "G"
+		user.Username = params["username"]
+		user.Password = "blackPowerRanger"
+	
+		// Creating new user and checking for error
+		if newUser := h.DB.Create(&user); newUser.Error != nil {
+			fmt.Println(newUser.Error)
+		}
+
+		json.NewEncoder(w).Encode(user)
+
+	} else {
+		w.Write([]byte("Username taken!"))
+	}
+}
