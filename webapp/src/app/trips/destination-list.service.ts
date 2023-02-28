03820ca8f9d2648f9b81f98468d0ca58729f3fe8
leonleonardo <leonleonardo@mailbox.org>
2023-02-27 23:55:38 -0500

    add destination-list service

---
+++ webapp/src/app/trips/destination-list.service.ts
@@ -0,0 +1,10 @@
+import { Destination } from 
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DestinationListService {
+
+  constructor() { }
+}
