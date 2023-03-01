42e325a1f68065039b77ac5fd38eb5a0cda6c3af
Richard Cusolito <richardcusolito1@gmail.com>
2023-03-01 01:54:28 -0500

    Connected signup page with backend

--- server/routes/CreateUser.go
+++ server/routes/CreateUser.go
@@ -14,7 +14,12 @@ func (h DBRouter) CreateUser(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
-	  }
+	}
+
+	type SignupAttempt struct {
+		//Signedup bool   `json: "loggedin"`
+		Message string `json: "message"`
+	}
 
 	// Creating two new variables to use as reference
 	var requestBody map[string]interface{}
@@ -28,7 +33,7 @@ func (h DBRouter) CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Added a line to check the database for any users with the same email as the new account
-	
+
 	result := h.DB.First(&models.User{}, "Email = ?", requestBody["Email"].(string))
 
 	// Checking if the rows that have the email is 0 therefore nobody has the email
@@ -36,7 +41,7 @@ func (h DBRouter) CreateUser(w http.ResponseWriter, r *http.Request) {
 		// Assigning Email and Password to new User
 		user.Email = requestBody["Email"].(string)
 		user.Password = requestBody["Password"].(string)
-	
+
 		// Creating new user in the DB and checking for error
 		if newUser := h.DB.Create(&user); newUser.Error != nil {
 			fmt.Println(newUser.Error)
@@ -45,20 +50,31 @@ func (h DBRouter) CreateUser(w http.ResponseWriter, r *http.Request) {
 		// Setting headers
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		
+
 		// Not generating accurate response data(?), looking to meet to come to concensus on how exactly
 		// we plan on passing information back and forth
 		//
-		// Essentially I would be encoding some sort of response with 
+		// Essentially I would be encoding some sort of response with
 		// "json.NewEncoder(w).Encode(response)"
 		//
 		// For now, I'm just printing validation strings.
-
-		w.Write([]byte("New user successfully created."))
+		response := SignupAttempt{Message: "User succesfully created account"}
+		jsonResponse, err1 := json.Marshal(response)
+		if err1 != nil {
+			http.Error(w, err1.Error(), http.StatusBadRequest)
+			return
+		}
+		w.Write(jsonResponse)
 
 	} else {
 		// If Rows Affected (rows with email given) is greater than 0, therefore someone has an account with
 		// the email given, we don't create a new user and tell them their email is taken.
-		w.Write([]byte("Email taken!"))
+		response := SignupAttempt{Message: "Email is already in use"}
+		jsonResponse, err2 := json.Marshal(response)
+		if err2 != nil {
+			http.Error(w, err2.Error(), http.StatusBadRequest)
+			return
+		}
+		w.Write(jsonResponse)
 	}
 }
--- webapp/src/app/signup/signup.component.ts
+++ webapp/src/app/signup/signup.component.ts
@@ -1,9 +1,37 @@
 import {Component} from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'signup-component',
   templateUrl: 'signup.component.html',
   styleUrls: ['signup.component.scss'],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  email!: string;
+  password!: string;
+  password1!: string;
+
+  constructor(private http: HttpClient) {}
+
+  createUser() {
+    if ((this.password == this.password1) && (this.email != null || this.password != null)){
+      const params = { Email: this.email, Password: this.password }
+
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      };
+
+      this.http.post("http://localhost:8181/createUser", params, httpOptions)
+      .subscribe(response => {
+        console.log(response);
+      });
+    } else if (this.email == null || this.password == null){
+      console.log("Email or Password is empty!");
+    } else {
+      console.log("Passwords don't match!");
+    }
+    
+  }
+}
